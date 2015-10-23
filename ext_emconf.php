4a6e6fa621480a71406ec7e729379e4221d52397
p251355 <p251355@localdomain>
2015-10-23 08:55:13 +0200

    adjust ext_config for TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,30 @@
 <?php
-    $EM_CONF[$_EXTKEY] = array(
-        'title' => 'Mr.Base-Configuration',
-        'description' => 'This Extension adds the Function to use an ext_configuration.php File.',
-        'category' => 'misc',
-        'author' => 'Sascha Heilmeier',
-        'author_email' => 'sheilmeier@gmail.com',
-        'author_company' => '',
-        'shy' => '',
-        'priority' => '',
-        'module' => '',
-        'state' => 'test',
-        'internal' => '',
-        'uploadfolder' => '0',
-        'createDirs' => '',
-        'modify_tables' => '',
-        'clearCacheOnLoad' => 0,
-        'lockType' => '',
-        'version' => '0.1',
-        'constraints' => array(
-            'depends' => array(
-                'cms'			=> '6.2-7.5',
-            ),
-            'conflicts' => array(
-            ),
-            'suggests' => array(
-            ),
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Mr.Base-Configuration',
+    'description' => 'This Extension adds the Function to use an ext_configuration.php File.',
+    'category' => 'misc',
+    'version' => '0.1.0',
+    'author' => 'Sascha Heilmeier',
+    'author_email' => 'sheilmeier@gmail.com',
+    'author_company' => '',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'test',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+	'constraints' => 
+	array (
+		'depends' => array (
+			'typo3' => '6.2.0-7.4.99',
         ),
-    );
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
